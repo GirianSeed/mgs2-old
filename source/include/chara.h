bdb1f40f62b95f745e2d81e05e4edfb41dfe3ce0
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2025-02-17 19:57:02 -0500

    Comment on empty params

--- source/include/chara.h
+++ source/include/chara.h
@@ -3,6 +3,9 @@
 
 #include "strcode.h"
 
+// XXX The empty parameter list is widely regarded as one of old C's
+// XXX anti-features. C++ and C23 have changed its meaning to (void).
+
 typedef void *(*NEWCHARA)();
 
 typedef struct {
