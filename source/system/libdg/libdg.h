e9bc497204e23276ff0d84c51dad97b938d46f9b
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2025-02-19 10:06:21 -0500

    Create libdg.h

---
+++ source/system/libdg/libdg.h
@@ -0,0 +1,14 @@
+#ifndef __MGS2_LIBDG_H__
+#define __MGS2_LIBDG_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* dgd.c */
+void DG_StartDaemon( void );
+
+#ifdef __cplusplus
+}
+#endif
+#endif // {{{ END OF FILE }}}
