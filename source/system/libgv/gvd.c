582406a7de0a51a5608a5a28c94578cf2487419a
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2025-08-24 22:08:49 -0400

    add libgv placeholders

---
+++ source/system/libgv/gvd.c
@@ -0,0 +1,7 @@
+#include "common.h"
+#include "libgv.h"
+
+void GV_StartDaemon( void )
+{
+    // todo: decompile
+}
---
+++ source/system/libgv/memlist.h
@@ -0,0 +1,17 @@
+#ifndef __MGS2_MEMLIST_H__
+#define __MGS2_MEMLIST_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//
+//
+//
+//
+//
+
+#ifdef __cplusplus
+}
+#endif
+#endif // {{{ END OF FILE }}}
