41e137ae157513f0258dcfddc7cbf9b096ced30e
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2025-08-26 20:16:43 -0400

    Update mgs_types.h
    For better portability and MSVC compat.

--- source/include/mgs_types.h
+++ source/include/mgs_types.h
@@ -1,9 +1,21 @@
 #ifndef __MGS2_MGS_TYPES_H__
 #define __MGS2_MGS_TYPES_H__
 
+#include <stddef.h>     // for size_t
+#include <limits.h>     // for MIN/MAX
 #include <sys/types.h>
-#include <eetypes.h>
 #include "mgs_defs.h"
+
+#ifdef __R5900__
+#include <eetypes.h>    // for u_long128
+#endif
+#ifdef _MSC_VER
+// MSVC's <sys/types.h> doesn't have these.
+typedef unsigned char   u_char;
+typedef unsigned short  u_short;
+typedef unsigned int    u_int;
+typedef unsigned long   u_long;
+#endif
 
 /*---------------------------------------------------------------------------*/
 // NOTE: (bool)0.5 evaluates to true, whereas (int)0.5 evaluates to 0.
@@ -25,13 +37,32 @@ typedef signed short    int16;          /* 16-bit signed integer    */
 typedef unsigned short  u_int16;        /* 16-bit unsigned integer  */
 typedef signed int      int32;          /* 32-bit signed integer    */
 typedef unsigned int    u_int32;        /* 32-bit unsigned integer  */
+#if (defined(__R5900__) || defined(__LP64__))
 typedef signed long     int64;          /* 64-bit signed integer    */
 typedef unsigned long   u_int64;        /* 64-bit unsigned integer  */
-typedef signed long     long64;         /* 64-bit signed integer    */
-typedef unsigned long   u_long64;       /* 64-bit unsigned integer  */
+#elif defined(_MSC_VER)
+typedef signed __int64  int64;          /* 64-bit signed integer    */
+typedef unsigned __int64 u_int64;       /* 64-bit unsigned integer  */
+#else
+typedef signed long long int64;         /* 64-bit signed integer    */
+typedef unsigned long long u_int64;     /* 64-bit unsigned integer  */
+#endif
+typedef int64           long64;         /* 64-bit signed long       */
+typedef u_int64         u_long64;       /* 64-bit unsigned long     */
 
+#if !defined(__R5900__)
+#if (defined(__GNUC__) && defined(__SIZEOF_INT128__))
 // https://gcc.gnu.org/onlinedocs/gcc/_005f_005fint128.html
 // https://gcc.gnu.org/onlinedocs/gccint/Machine-Modes.html
+
+typedef   signed int long128    __attribute__((mode(TI)));
+typedef unsigned int u_long128  __attribute__((mode(TI)));
+#else
+// One of several ways to make a fake 128-bit integer.
+typedef struct { u_long64 hi,lo; } long128;
+typedef struct { u_long64 hi,lo; } u_long128;
+#endif
+#endif // !defined(__R5900__)
 
 typedef long128         int128;         /* 128-bit signed integer   */
 typedef u_long128       u_int128;       /* 128-bit unsigned integer */
